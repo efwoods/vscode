539e7a07aa09bb0b6c09b1a522b449d8bcedfb84
efwoods <efwoods@g.clemson.edu>
2020-03-21 19:31:55 -0700

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CODE=$(which code)
+if [ -z $CODE ]; then
+    sudo apt update
+    sudo apt install software-properties-common apt-transport-https wget
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+    sudo apt install code
+else
+    echo "Visual Studio Code is already installed @: $CODE"
+fi
